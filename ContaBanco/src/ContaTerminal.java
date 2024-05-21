428cc875d449b53de4eb150c291f61163502cb6e
Thais-Abe <abe.c.thais@gmail.com>
2024-05-21 11:03:25 -0300

    adicionando resolução do problema proposto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,55 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.println("iniciando o projeto");
+
+    private int numero;
+    private String agencia;
+    private String Nome_cliente;
+    private double saldo;
+
+    public ContaTerminal(String nome, String agencia, int numero, double saldo ) {
+        this.numero = numero;
+        this.agencia = agencia;
+        Nome_cliente = nome;
+        this.saldo = saldo;
+    }
+
+    public ContaTerminal() {
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNome_cliente() {
+        return Nome_cliente;
+    }
+
+    public void setNome_cliente(String nome_cliente) {
+        Nome_cliente = nome_cliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Olá " + getNome_cliente() + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                getAgencia() + ", conta " + getNumero() + "e seu saldo " + getSaldo() + " já esta disponível para saque";
     }
 }
---
+++ ContaBanco/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scam = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scam.nextLine();
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scam.nextLine();
+        System.out.println("Entre com o número da sua conta: ");
+        int numeroConta = scam.nextInt();
+        System.out.println("Deseja realizar algum depósito? ");
+        char resposta = scam.next().charAt(0);
+        scam.nextLine();
+        double deposito;
+        if (resposta == 's'){
+            System.out.println("Qual a quantia que deseja depositar?");
+            deposito = scam.nextDouble();
+        }else {
+            deposito = 0.00;
+        }
+        ContaTerminal conta = new ContaTerminal(nome, agencia,numeroConta,deposito);
+        System.out.println(conta.toString());
+
+
+        scam.close();
+    }
+}
